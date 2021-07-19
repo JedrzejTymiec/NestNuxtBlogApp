463c8663c5383c934d3eae7e19c02981de000d04
Jedrzej Tymiec <jedrek1692@gmail.com>
2021-07-19 22:39:36 +0200

    client init

---
+++ client/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <Navbar />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>HELLO</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
